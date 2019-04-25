fd537c6a3ddb99e4eaad63d7adc0fd644e9240b6
JensOAndersen <jensoleand@gmail.com>
2019-04-25 14:06:48 +0200

    Completed CommonDBAccess

--- DataAccess/CommonDBAccess.cs
+++ DataAccess/CommonDBAccess.cs
@@ -1,8 +1,34 @@
 ﻿using System;
+using System.Data;
+using System.Data.SqlClient;
 
 namespace DataAccess
 {
     public class CommonDBAccess
     {
+        private const string ConString = @"Data Source=(localdb)\MSSQLLocalDB;Initial Catalog=RecipeRP;Integrated Security=True;Connect Timeout=30;Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False";
+    
+        public DataTable ExecuteQuery(string q)
+        {
+            DataTable dt = new DataTable();
+
+            using (SqlDataAdapter dap = new SqlDataAdapter(q,ConString))
+            {
+                dap.Fill(dt);
+            }
+
+            return dt;
+        }
+
+        public int ExecuteNonQuery(string q)
+        {
+            using(SqlConnection con = new SqlConnection(ConString))
+            using(SqlCommand com = new SqlCommand(q, con))
+            {
+                con.Open();
+
+                return com.ExecuteNonQuery();
+            }
+        }
     }
 }
---
+++ DataAccess/DBAccess.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Entities;
+
+namespace DataAccess
+{
+    public class DBAccess : CommonDBAccess
+    {
+    }
+}
