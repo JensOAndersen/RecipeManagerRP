0d3b8f1133fa8f6839f529666b102962f4786ddc
JensOAndersen <jensoleand@gmail.com>
2019-04-25 14:11:59 +0200

    Completed GetAllIngredients

--- DataAccess/DBAccess.cs
+++ DataAccess/DBAccess.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data;
 using System.Text;
 using Entities;
 
@@ -7,5 +8,26 @@ namespace DataAccess
 {
     public class DBAccess : CommonDBAccess
     {
+        public List<Ingredient> GetAllIngredients()
+        {
+            string q = "SELECT * FROM Ingredients";
+            List<Ingredient> ingredients = new List<Ingredient>();
+
+            DataTable dt = ExecuteQuery(q);
+
+            foreach (DataRow row in dt.Rows)
+            {
+                ingredients.Add(
+                    new Ingredient()
+                    {
+                        Id = (int)row["Id"],
+                        Name = (string)row["Name"],
+                        Calories = (int)row["Calories"]
+                    }
+                );
+            }
+
+            return ingredients;
+        }
     }
 }
