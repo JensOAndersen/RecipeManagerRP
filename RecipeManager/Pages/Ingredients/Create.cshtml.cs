fcbbab31d59c9f865daf2a30fffbccda18d88e54
JensOAndersen <jensoleand@gmail.com>
2019-04-26 08:34:00 +0200

    Added form for Ingredient Creation

--- RecipeManager/Pages/Ingredients/Create.cshtml.cs
+++ RecipeManager/Pages/Ingredients/Create.cshtml.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Entities;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
@@ -9,7 +10,15 @@ namespace RecipeManager.Pages.Ingredients
 {
     public class CreateModel : PageModel
     {
+        [BindProperty]
+        public Ingredient Ingredient { get; set; }
+
         public void OnGet()
+        {
+
+        }
+
+        public void OnPost()
         {
 
         }
