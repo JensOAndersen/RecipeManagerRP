691c7ce6abe55acdba4d41404125dae7540781b3
JensOAndersen <jensoleand@gmail.com>
2019-04-26 11:39:37 +0200

    Added Detail and Create Pages

---
+++ RecipeManager/Pages/Recipes/Detail.cshtml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using DataAccess;
+using Entities;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace RecipeManager.Pages.Recipes
+{
+    public class DetailModel : PageModel
+    {
+        RecipeRepository repository;
+
+        public Recipe Recipe { get; set; }
+
+        public DetailModel()
+        {
+            repository = new RecipeRepository();
+        }
+
+        public void OnGet(int id)
+        {
+            Recipe = repository.GetRecipe(id);
+        }
+    }
+}
