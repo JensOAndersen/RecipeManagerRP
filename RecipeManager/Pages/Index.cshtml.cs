d542dad7c39f7a813dd4e2085900ff3d095ec526
JensOAndersen <jensoleand@gmail.com>
2019-04-28 17:13:53 +0200

    moved recipe/detail view into main index

--- RecipeManager/Pages/Index.cshtml.cs
+++ RecipeManager/Pages/Index.cshtml.cs
@@ -2,6 +2,8 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using DataAccess;
+using Entities;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
@@ -9,9 +11,14 @@ namespace RecipeManager.Pages
 {
     public class IndexModel : PageModel
     {
+        RecipeRepository repository;
+
+        public List<Recipe> Recipes { get; set; }
         public void OnGet()
         {
+            repository = new RecipeRepository();
 
+            Recipes = repository.GetAllRecipes();
         }
     }
 }
